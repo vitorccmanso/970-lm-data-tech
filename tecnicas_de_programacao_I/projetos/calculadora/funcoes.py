134908391c704d160843f34066dc5c1983266913
vitorccmanso <vitorccmanso@hotmail.com>
2023-03-29 15:26:36 -0300

    Função Sub

--- tecnicas_de_programacao_I/projetos/calculadora/funcoes.py
+++ tecnicas_de_programacao_I/projetos/calculadora/funcoes.py
@@ -4,3 +4,10 @@ def soma(a, b):
         return float(a) + float(b)
     except (ValueError, TypeError):
         raise TypeError("O input 'a' e 'b' devem ser numeros inteiros, ou decimais")
+
+#Função subtração
+def sub(a, b):
+    try:
+        return float(a) - float(b)
+    except (ValueError, TypeError):
+        raise TypeError("O input 'a' e 'b' devem ser numeros inteiros, ou decimais")
